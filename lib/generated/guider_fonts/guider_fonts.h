7cc547cdab0ea291cc726abaf1b1b17ae107066e
light <lightseraph@163.com>
2022-09-17 23:02:27 +0800

    init

---
+++ lib/generated/guider_fonts/guider_fonts.h
@@ -0,0 +1,20 @@
+
+    #ifndef GUIDER_FONTS_H
+    #define GUIDER_FONTS_H
+    #ifdef __cplusplus
+    extern "C" {
+    #endif
+    
+    #include "lv_font.h"
+    
+    LV_FONT_DECLARE(lv_font_arial_20)
+LV_FONT_DECLARE(lv_font_arial_16)
+LV_FONT_DECLARE(lv_font_arial_23)
+LV_FONT_DECLARE(lv_font_simsun_20)
+
+    
+    #ifdef __cplusplus
+    }
+    #endif
+    #endif
+    
